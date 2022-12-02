f5f77e352d21e5f65c23618d07d1ce6defb6f56c
Lubna Tahreem <lubnatahreem789@gmail.com>
2022-12-02 17:31:33 +0530

    created a react page

--- chatapp/src/index.js
+++ chatapp/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <h1>Hello</h1>
+  <div>
+ <h1>Hello Everyone</h1>
+ <h2>Iam Lubna Tahreem</h2>
+ 
+ <img class="" src='https://i.pinimg.com/originals/de/99/93/de9993e752fc52646579448542c411d3.jpg' alt='im'></img>
+  </div>
+ 
 );
